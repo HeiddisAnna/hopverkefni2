5dc0836c06b89a23d788028870cad891789f6cd5
HeiddisAnna <heiddisanna92@gmail.com>
2018-11-25 17:46:21 +0000

    fyrsta tilraun af gögnum á næstu síðu

--- src/index.js
+++ src/index.js
@@ -6,13 +6,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const isLecturePage = page.classList.contains('lecture-page');
 
   if (isLecturePage) {
-    console.log('er í lecture page');
    const lecture = new Lecture();
    lecture.load();
    
   } else {
-    console.log(isLecturePage);
-    console.log('er á hinni síðunni');
     const list = new List();
     list.load();
   } 
--- src/lib/lecture.js
+++ src/lib/lecture.js
@@ -4,6 +4,7 @@ import { loadSavedLectures, savedLectures,  } from './storage';
 export default class List {
     constructor() {
       this.container = document.querySelector('.lecture-page');
+      this.containerList = document.querySelector('.lecture__list');
       this.url='../lectures.json';
     }
 
@@ -18,17 +19,36 @@ export default class List {
         const saved = loadSavedLectures();
 
         return saved;
-
+    }
+    
+    loadLectures(slug) {
+      return fetch(this.url)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Villa við að sækja fyrirlestra');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        const found = data.lectures.find(i => i.slug === slug);
+        if(!found){
+          throw new Error('Fyrirlestur fannst ekki');
+        }
+        return found;
+      });
     }
   
     load() {
-      console.log('Fer í load í lecture');
-
-      //empty(this.container);
       const qs = new URLSearchParams(window.location.search);
-      console.log(qs);
       const slug = qs.get('slug');
-      console.log(slug);
+
+      this.loadLectures(slug)
+      .then(data => this.showList(data)) //Sendum lista af öllum gögnunum inn í fallið
+      
+      .catch((error) => {
+        console.error(error);
+        this.setError(error.message);
+      }); 
 
       //Villumeðhöndlun - ef síðan fannst ekki
 
@@ -41,6 +61,27 @@ export default class List {
       finished.setAttribute('href', '/fyrirlestur.html?slug=' +slug); //þarf að laga þetta
       finished.addEventListener('click', this.finished); 
       this.container.appendChild(finished);
- 
+    }
+
+    show(data){
+      console.log(data.data);
+      console.log(data.type == 'text');
+      
+      if(data.type == 'text'){
+        const newElement = el('p', data.data);
+        newElement.classList.add(data.type);
+        this.containerList.appendChild(newElement);
+      }
+
+    }
+
+    showList(data){
+      //console.log(data.content);
+      //console.log(data.content.length);
+
+      for(let i=0; i< data.content.length; i++){
+        this.show(data.content[i]);
+      }
+    
     }
 }
